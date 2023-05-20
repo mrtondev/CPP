6f664bf4c43f628eb786824f65976879a2a60216
Ayrton Rafael <ayrton.rafael@gmail.com>
2023-05-20 01:56:08 -0300

    Default

---
+++ _cal_Switch_.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <locale>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ptb");
+
+    int numA, numB, res;
+    char mat;
+    bool des;
+
+    des == true;
+    cout << "--- Bem-vindo   ---" << endl;
+    cout << "---     à       ---" << endl;
+    cout << "--- Calculadora ---" << endl;
+    cout << "\n";
+    cout << "Digite o primeiro número: \n";
+    cin >> numA;
+    cout << "Agora o segundo número: \n";
+    cin >> numB;
+    cout << "Agora o operador matemático \n";
+    cout << "( +  -  *  / ) \n";
+    cin >> mat;
+
+    switch (mat)
+    {
+    case '+':
+    {
+        res = numA + numB;
+        cout << numA << "+" << numB << " = " << res << endl;
+        break;
+    }
+    case '-':
+    {
+        res = numA - numB;
+        cout << numA << "-" << numB << " = " << res << endl;
+        break;
+    }
+    case '*':
+    {
+        res = numA * numB;
+        cout << numA << "*" << numB << " = " << res << endl;
+        break;
+    }
+    case '/':
+    {
+        res = numA / numB;
+        cout << numA << " / " << numB << " =" << res << endl;
+        break;
+    }
+
+    default:
+        cout << "Erro! Operador inválido!" << endl;
+    }
+}
